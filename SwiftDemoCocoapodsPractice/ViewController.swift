7dd9fff398196d47fc359e6b8882cbd2a7331b72
mad-hu <450287549@qq.com>
2016-06-17 11:22:44 +0800

    Initial Commit

---
+++ SwiftDemoCocoapodsPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemoCocoapodsPractice
+//
+//  Created by yandonghu on 16/6/17.
+//  Copyright © 2016年 Mad-hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
